0bdcd5605553e418824e5afc1ba837589f85afc5
Sam Leach <samuelleach78@webster.edu>
2020-01-22 15:35:20 -0600

    Week 1 work (1-22-20)
    Wrote a new Character Controller and Player movement script based off of a Brackey's tutorial (V3PlayerCharacterController and CharacterController2D)
    Implemented new horizontal movement and jumping.

---
+++ Firewall/Assets/Scripts/Movement/PlayerCharacterControllerV2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterControllerV2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    Vector2 previousPosition;
+    Vector2 currentPosition;
+    Vector2 nextMovement;
+    Vector2 jump_scalar;
+  
+    bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        Vector2 currentPosition = rb.position;
+        Vector2 previousPosition = rb.position;
+
+        nextMovement = new Vector2(1, 0);
+        jump_scalar = new Vector2(0, 1);
+    }
+
+    //Fixed update is called a set amount of times per second
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            nextMovement = new Vector2(-1, 0);
+            Move(nextMovement);
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            nextMovement = new Vector2(1, 0);
+            Move(nextMovement);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += jump_scalar;
+        }
+
+    }
+
+    public void Move(Vector2 m)
+    {
+        previousPosition = rb.position;
+        currentPosition = previousPosition + m;
+
+        rb.position = currentPosition;
+    }
+
+    public void Jump()
+    {
+
+    }
+}
