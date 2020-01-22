501d9c8537d4e0a6760606288bb876d2ca6c5429
Sam Leach <samuelleach78@webster.edu>
2020-01-21 23:57:32 -0600

    Week 1 Sprint (1-21-20)
    Created Unity project for Firewall
    Created Player game object
    Wrote PlayerControllerV2 script, allowing for horizontal player movement
    Beginning implementation of jump

---
+++ Firewall/Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public Vector2 movementSpeed;
+    bool isMoving;
+    private float moveTime = 0.1f;
+    public bool onCooldown = false;
+
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //StartCoroutine(actionCooldown(0.1f));
+            Move(-movementSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //StartCoroutine(actionCooldown(0.1f));
+            Move(movementSpeed);
+        }
+
+    }
+
+    private void Move(Vector2 m)
+    {
+        Vector2 startPosition = transform.position;
+        Vector2 endPosition = startPosition + m;
+
+        rb.position = endPosition;
+
+        StartCoroutine(SmoothMovement(endPosition));
+    }
+
+    private IEnumerator SmoothMovement(Vector3 end)
+    {
+        //while (isMoving) yield return null;
+
+        isMoving = true;
+
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        float inverseMoveTime = 1 / moveTime;
+
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, end, inverseMoveTime * Time.deltaTime);
+            transform.position = newPosition;
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
+    private IEnumerator actionCooldown(float cooldown)
+    {
+        onCooldown = true;
+
+        //float cooldown = 0.2f;
+        while (cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+            yield return null;
+        }
+
+        onCooldown = false;
+    }
+}
