e6e62a575f7b57cf91baf5f15f93d67e48eebf81
therealtboc <55810542+therealtboc@users.noreply.github.com>
2020-01-22 20:23:17 -0600

    Class 2 (1-22-20)
    Added PlayerHealth script (contains ppublic health integer)

---
+++ Firewall/Assets/Scripts/Movement/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
