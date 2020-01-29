12ac770a2583ef223cfff545944863061088077d
Sam Leach <samuelleach78@webster.edu>
2020-01-29 17:06:18 -0600

    1-29-20 Updates
    Added a testing area for double jumping
    Modified the comments on CharacterController2D and V3PlayerController to better define the areas
    Created a testing environment for Enemy AI
    Implemented "Enemy" and "Environment" tags for use on Colliders
    Created a rudimentary Enemy object within this new scene (currently does not work)

---
+++ Firewall/Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//EnemyController is a test script that allows for AI control of an enemy object
+public class EnemyController : MonoBehaviour
+{
+    //****************************************************************** Variable Declaration ******************************************************************
+    public CharacterController2D enemyController;
+    public float enemyRunSpeed = 30f;
+    public float horizontalMove = 1f;
+       
+    private bool groundedCheck = true;
+
+    //****************************************************************** Start function ******************************************************************
+    void Start()
+    {
+        //horizontalMove *= enemyRunSpeed;
+    }
+
+    //****************************************************************** Update function ******************************************************************
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //****************************************************************** FixedUpdate function ******************************************************************
+    void FixedUpdate()
+    {
+        horizontalMove = horizontalMove * enemyRunSpeed;
+        //Debug.Log(horizontalMove);
+        horizontalMove = 1;
+
+        groundedCheck = enemyController.getGrounded();
+        Debug.Log(groundedCheck + " " + horizontalMove);
+        if (groundedCheck)
+        {
+            Debug.Log("we're moving");
+            enemyController.Move(horizontalMove * Time.fixedDeltaTime, false);
+        }
+
+        //enemyController.Move(horizontalMove * Time.fixedDeltaTime, false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.tag == "Environment")
+        //{
+        //    horizontalMove *= -1;
+        //}
+    }
+}
+
+//****************************************************************** END OF CODE ******************************************************************
--- Firewall/Assets/Scripts/Movement/CharacterController2D.cs
+++ Firewall/Assets/Scripts/Movement/CharacterController2D.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+//The CharacterController2D class is a script that allows for horizontal movement, jumping, crouching, and flipping the player to accurately move. This should be added to any entity that moves.
 public class CharacterController2D : MonoBehaviour
 {
+    //****************************************************************** Variable Declaration ******************************************************************
+
     [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
     [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
@@ -33,6 +36,8 @@ public class CharacterController2D : MonoBehaviour
     public BoolEvent OnCrouchEvent;
     private bool m_wasCrouching = false;
 
+    //****************************************************************** Awake function ******************************************************************
+
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
@@ -43,6 +48,8 @@ public class CharacterController2D : MonoBehaviour
         if (OnCrouchEvent == null)
             OnCrouchEvent = new BoolEvent();
     }
+
+    //****************************************************************** FixedUpdate function ******************************************************************
 
     private void FixedUpdate()
     {
@@ -64,6 +71,9 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
+    //****************************************************************** Move function ******************************************************************
+    //The move function gives horizontal movement functionality to the associated entity. It contains the logic for movement (velocity-based), jumping (velocity-based), and will call the Flip function for correct movement
+    //NOTE - the code referenced has the logic for crouching - this is currently disabled.
 
     public void Move(float move, bool crouch/*, bool jump*/)
     {
@@ -77,10 +87,12 @@ public class CharacterController2D : MonoBehaviour
             }
         }
 
+        //******************** OVERARCHING MOVEMENT LOGIC ********************
+
         //only control the player if grounded or airControl is turned on
         if (m_Grounded || m_AirControl)
         {
-
+            //******************** CROUCHING ********************
             // If crouching
             if (crouch)
             {
@@ -110,6 +122,8 @@ public class CharacterController2D : MonoBehaviour
                 }
             }
 
+            //******************** HORIZONTAL MOVEMENT ********************
+
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
@@ -128,6 +142,9 @@ public class CharacterController2D : MonoBehaviour
                 Flip();
             }
         }
+
+        //******************** JUMPING ********************
+
         // If the player should jump...
         if (m_Grounded && /*jump*/ Input.GetButtonDown("Jump"))
         {
@@ -147,6 +164,8 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
+    //****************************************************************** Flip function ******************************************************************
+    //The Flip function will change the direction the entity is facing.
 
     private void Flip()
     {
@@ -158,5 +177,15 @@ public class CharacterController2D : MonoBehaviour
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+
+    //****************************************************************** getGrounded function ******************************************************************
+    //The getGrounded function returns the value of private bool m_grounded, allowing other scripts to glean if this entity is on the ground.
+
+    public bool getGrounded()
+    {
+        return m_Grounded;
+    }
 }
+
+//****************************************************************** END OF CODE ******************************************************************
 
