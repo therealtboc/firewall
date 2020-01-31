8f7c702639636a168e151af6c5414e596d316c9f
Sam Leach <samuelleach78@webster.edu>
2020-01-30 21:42:22 -0600

    1-30-20 Work
    Fixed enemy targeting to be regular
    Added environmental colliders to first part of tutorial stage
    Beginning work on attack functions
    General commenting and code cleanup

--- Firewall/Assets/Scripts/Enemy Scripts/EnemyController.cs
+++ Firewall/Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //EnemyController is a test script that allows for AI control of an enemy object
+//Current behaviors include: patrolling (with environment collisions), player targeting
 public class EnemyController : MonoBehaviour
 {
     //****************************************************************** Variable Declaration ******************************************************************
@@ -11,11 +12,16 @@ public class EnemyController : MonoBehaviour
 
     public CharacterController2D enemyController;
     public float enemyRunSpeed = 30f;
+    public float enemyChaseSpeed = 5f;
     public float horizontalMove = 1f;
 
     private float changeDirection = 1f;
+    public float enemyDistance = 10f;
+
+    private Vector3 setEnemyDistance;
 
     bool playerFound = false;
+    bool canAttack = false;
        
     private bool groundedCheck = true;
 
@@ -24,6 +30,8 @@ public class EnemyController : MonoBehaviour
     {
         //horizontalMove *= enemyRunSpeed;
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        setEnemyDistance = new Vector3(enemyDistance, 0, 0);
     }
 
     //****************************************************************** Update function ******************************************************************
@@ -32,7 +40,8 @@ public class EnemyController : MonoBehaviour
     {
         if (playerFound)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, enemyRunSpeed * Time.deltaTime); //change speed
+            //When the playerFound bool is true, the entity will track to the position of the player
+            transform.position = Vector2.MoveTowards(transform.position, target.position, enemyChaseSpeed * Time.deltaTime); //essentially make the target vector3 the target.position - buffer
         }
     }
 
@@ -55,20 +64,41 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    //****************************************************************** COLLISION DETECTION ******************************************************************
+
+    //****************************************************************** OnCollisionEnter2D function ******************************************************************
+    //This function defines behavior once another entity with a collider enters a collision with this entity's colliders
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Collision must be with a collider tagged as Environment
         if (collision.gameObject.tag == "Environment")
         {
+            //This reverses the direction of the entity
             changeDirection *= -1f;
         }
     }
 
+    //****************************************************************** OnTriggerEnter2D function ******************************************************************
+    //This function defines behavior once another entity with a collider enters an IsTrigger collision with this entity's colliders
+    //Our radius for the enemy is an IsTrigger collision, meaning it will begin targeting the player when the radius is touched
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Collision must be with the player's collider
         if (collision.gameObject.tag == "Player")
         {
             playerFound = true;
             //Debug.Log("player found");
+        }
+    }
+
+    //****************************************************************** OnTriggerExit2D function ******************************************************************
+    //This function defines behavior once another entity with a collider exits an IsTrigger collision with this entity's colliders
+    //Our radius for the enemy is an IsTrigger collision, meaning it will stop targeting the player when the radius is exited
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerFound = false;
         }
     }
 }
